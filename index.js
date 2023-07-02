8bddb3546fa1b7b1ffb9ba0abb4e712bcb30830c
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-02 16:00:01 +0700

    Set up Mongoose connection to MongoDB database in index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
-
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -27,3 +26,15 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
