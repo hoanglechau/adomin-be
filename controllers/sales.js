792b6bbd97c833f4a9dbe06f9748e4a93598a272
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-07-23 11:30:12 +0700

    Update the sales controllers

--- controllers/sales.js
+++ controllers/sales.js
@@ -2,10 +2,9 @@ import OverallStat from "../models/OverallStat.js";
 
 export const getSales = async (req, res) => {
   try {
-    const OverallStats = await OverallStat.find();
-
+    const overallStats = await OverallStat.find();
     // Only sending the first version of 2021
-    res.status(200).json(OverallStats[0]);
+    res.status(200).json(overallStats[0]);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
